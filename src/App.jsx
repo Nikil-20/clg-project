7e81a919dfcc865d37ef8dd960f04a9093720c5f
nikil-20 <nikilgajankush@gmail.com>
2024-10-14 19:53:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div> hello from coep college </div>
+    </>
+  )
+}
+
+export default App
